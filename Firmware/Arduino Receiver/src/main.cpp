8d244777d3d77bc64acba486ebc49dcef7bafbe7
Ahmed-Mohiuddin-Shah <mohiuddinlive@gmail.com>
2024-05-20 02:28:14 +0500

    feat: Update platformio.ini to include RadioHead and LiquidCrystal_I2C libraries

--- Firmware/Arduino Receiver/src/main.cpp
+++ Firmware/Arduino Receiver/src/main.cpp
@@ -1,18 +1,25 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#include <SPI.h>
+#include <Adafruit_I2CDevice.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
 
-void setup() {
+#define I2C_ADDR 0x27
+
+LiquidCrystal_I2C lcd(I2C_ADDR, 16, 2);
+
+void setup()
+{
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(9600);
+  lcd.init();
+  lcd.backlight();
+  lcd.setCursor(0, 0);
+  lcd.print("Hello, world!");
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
 }
