f77d87b840f8dd222581ba1fe192822526b70a27
Ahmed-Mohiuddin-Shah <mohiuddinlive@gmail.com>
2024-05-20 01:43:27 +0500

    feat: Update platformio.ini to include RadioHead library
    The code changes in the `platformio.ini` file add the `mikem/RadioHead` library as a dependency for both the Arduino Receiver and Transmitter. This library provides support for RF communication, which is essential for the functionality of the project. Including the library ensures that the necessary functions and classes are available for transmitting and receiving data wirelessly.
    
    Recent user commits:
    - Initialize timer in transmitter.cpp
    - Update transmitter.cpp to fix whitespace issue
    - Update LCD display menu function
    - Move Functions and declarations to functions.h
    - implement delay with timer
    - Implement keypad-based restaurant management system
    - Update README.md
    - Implement keypad-based restaurant management system
    
    Recent repository commits:
    - Update receiver.cpp to process and display orders
    - Update workspace configuration
    - Update platformio.ini to exclude specific cpp files

--- Firmware/Arduino Transmitter/src/main.cpp
+++ Firmware/Arduino Transmitter/src/main.cpp
@@ -25,6 +25,7 @@
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
+
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
 
