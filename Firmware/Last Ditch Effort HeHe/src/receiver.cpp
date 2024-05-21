8cc0609d96133ec92372ca5660b88247d6971091
Ahmed-Mohiuddin-Shah <mohiuddinlive@gmail.com>
2024-05-21 13:37:24 +0500

    feat: Update receiver.cpp to process and display orders

--- Firmware/Last Ditch Effort HeHe/src/receiver.cpp
+++ Firmware/Last Ditch Effort HeHe/src/receiver.cpp
@@ -1,86 +1,167 @@
 #include <Arduino.h>
-
-// ESP Board MAC Address : 9C : 9C : 1F : C4 : FA : 80
-
 #include <SPI.h>
 #include <Adafruit_I2CDevice.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
+#include <esp_now.h>
+#include <WiFi.h>
 
-#include <Keypad.h>
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+#define OLED_RESET -1       // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_ADDRESS 0x3C // See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
 
-#include <HardwareSerial.h>
+int lineNum = 0;
+int order_total = 0;
 
-#define SERIAL1 12
-#define SERIAL2 2
+typedef struct data_struct
+{
+    char key;
+} data_struct;
 
-                                            HardwareSerial ESPSerial1(SERIAL1);
+data_struct myData;
 
-#define COMPORT ESPSerial1
+// Function Prototypes
+void processOrder(char key);
+void OnDataRecv(const uint8_t *mac, const uint8_t *incomingData, int len);
 
-void comportInit(long baudrate);
-int comportReceive(byte *data, int length);
-int comportTransmit(byte *data, int length);
-
-#define MSGBUFFERSIZE 80
-byte pData[MSGBUFFERSIZE];
-
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 void setup()
 {
-    // put your setup code here, to run once:
-    Serial.begin(9600);
+    Serial.begin(115200);
 
-
-    comportInit(9600);
-
-    // driver = RH_ASK(uint16_t speed = 2000, uint8_t rxPin = 11, uint8_t txPin = 12, uint8_t pttPin = 10, bool pttInverted = false);
-
-    // RMT.begin();
-
-
-}
-
-void loop()
-{
-
-    comportReceive(pData, 2);
-
+    if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS))
+    {
+        Serial.println(F("SSD1306 allocation failed"));
+        for (;;);
+    }
+    display.display();
+    delay(2000);
     display.clearDisplay();
     display.setTextSize(1);
     display.setTextColor(SSD1306_WHITE);
     display.setCursor(0, 0);
-    // pData contents
+    display.display();
 
-    display.println(pData[0]);
-    display.println(pData[1]);
-
-    char key = keypad.getKey();
-
-    if (key)
+    WiFi.mode(WIFI_STA);
+    if (esp_now_init() != ESP_OK)
     {
-        Serial.println(key);
+        Serial.println("Error initializing ESP-NOW");
+        return;
     }
+    esp_now_register_recv_cb(OnDataRecv);
 }
 
-void comportInit(long baudrate)
+void loop()
 {
-    COMPORT.begin(baudrate);
+    // Display the text received
 }
 
-int comportReceive(byte *data, int length)
+void processOrder(char key)
 {
-    int i = 0;
-    while (COMPORT.available() > 0 && i < length)
+    char order[16];
+    switch (key)
     {
-        data[i] = COMPORT.read();
-        i++;
+    case '1':
+        strcpy(order, "Tea      Rs.100");
+        order_total += 100;
+        lineNum += 1;
+        break;
+    case '2':
+        strcpy(order, "Coffee   Rs.150");
+        order_total += 150;
+        lineNum += 1;
+        break;
+    case '3':
+        strcpy(order, "Biscuits  Rs.40");
+        order_total += 40;
+        lineNum += 1;
+        break;
+    case '4':
+        strcpy(order, "Cookies  Rs.250");
+        order_total += 250;
+        lineNum += 1;
+        break;
+    case '5':
+        strcpy(order, "Roti      Rs.30");
+        order_total += 30;
+        lineNum += 1;
+        break;
+    case '6':
+        strcpy(order, "Lunch    Rs.200");
+        order_total += 200;
+        lineNum += 1;
+        break;
+    case '7':
+        strcpy(order, "Dinner   Rs.250");
+        order_total += 250;
+        lineNum += 1;
+        break;
+    case '8':
+        strcpy(order, "BreakfastRs.150");
+        order_total += 150;
+        lineNum += 1;
+        break;
+    case '9':
+        strcpy(order, "Snacks   Rs.100");
+        order_total += 100;
+        lineNum += 1;
+        break;
+    case '0':
+        strcpy(order, "Water    Rs.20");
+        order_total += 20;
+        lineNum += 1;
+        break;
+    case 'C':
+        strcpy(order, "Order Confirmed");
+        display.clearDisplay();
+        display.setTextSize(1);
+        display.setTextColor(SSD1306_WHITE);
+        display.setCursor(0, 0);
+        display.println(order);
+        display.println("Order Total     ");
+        char total[16];
+        sprintf(total, "Rs.%d", order_total);
+        display.println(total);
+        display.display();
+        order_total = 0;
+        lineNum = 0;
+        return;
+        break;
+    case 'D':
+        strcpy(order, "Order Cancelled");
+        display.clearDisplay();
+        display.setTextSize(1);
+        display.setTextColor(SSD1306_WHITE);
+        display.setCursor(0, 0);
+        display.println(order);
+        display.display();
+        lineNum = 0;
+        order_total = 0;
+        break;
+    default:
+        return;
+        break;
     }
-    return i;
+    // display.clearDisplay();
+    display.setTextSize(1);
+    display.setTextColor(SSD1306_WHITE);
+    display.setCursor(0, 0);
+    for (int i = 0; i < lineNum; i++)
+    {
+        display.println("");
+    }
+    display.println(order);
+    display.display();
 }
 
-int comportTransmit(byte *data, int length)
+void OnDataRecv(const uint8_t *mac, const uint8_t *incomingData, int len)
 {
-    COMPORT.write(data, length);
-    return length;
+    memcpy(&myData, incomingData, sizeof(myData));
+    display.setTextSize(1);
+    display.setTextColor(SSD1306_WHITE);
+    display.setCursor(0, 0);
+    processOrder(myData.key);
+    display.display();
 }
--- Firmware/Last Ditch Effort HeHe/src/transmitter.cpp
+++ Firmware/Last Ditch Effort HeHe/src/transmitter.cpp
@@ -1,172 +1,17 @@
-// Complete Instructions to Get and Change ESP MAC Address: https://RandomNerdTutorials.com/get-change-esp32-esp8266-mac-address-arduino/
-
 #include <SPI.h>
 #include <Adafruit_I2CDevice.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
-
 #include <Keypad.h>
 #include <SPI.h>
 #include <Adafruit_I2CDevice.h>
-
-//=================
-
-int order_total = 0;
+#include <esp_now.h>
+#include <WiFi.h>
 
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
-
 #define OLED_RESET -1       // Reset pin # (or -1 if sharing Arduino reset pin)
 #define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
-
-Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
-
-int lineNum = 0;
-
-// Process Order Function
-void processOrder(char key)
-{
-    char order[16];
-    switch (key)
-    {
-    case '1':
-        strcpy(order, "Tea      Rs.100");
-        order_total += 100;
-        lineNum += 1;
-        break;
-    case '2':
-        strcpy(order, "Coffee   Rs.150");
-        order_total += 150;
-        lineNum += 1;
-        break;
-    case '3':
-        strcpy(order, "Biscuits  Rs.40");
-        order_total += 40;
-        lineNum += 1;
-        break;
-    case '4':
-        strcpy(order, "Cookies  Rs.250");
-        order_total += 250;
-        lineNum += 1;
-        break;
-    case '5':
-        strcpy(order, "Roti      Rs.30");
-        order_total += 30;
-        lineNum += 1;
-        break;
-    case '6':
-        strcpy(order, "Lunch    Rs.200");
-        order_total += 200;
-        lineNum += 1;
-        break;
-    case '7':
-        strcpy(order, "Dinner   Rs.250");
-        order_total += 250;
-        lineNum += 1;
-        break;
-    case '8':
-        strcpy(order, "BreakfastRs.150");
-        order_total += 150;
-        lineNum += 1;
-        break;
-    case '9':
-        strcpy(order, "Snacks   Rs.100");
-        order_total += 100;
-        lineNum += 1;
-        break;
-    case '0':
-        strcpy(order, "Water    Rs.20");
-        order_total += 20;
-        lineNum += 1;
-        break;
-    case 'C':
-        strcpy(order, "Order Confirmed");
-        display.clearDisplay();
-        display.setTextSize(1);
-        display.setTextColor(SSD1306_WHITE);
-        display.setCursor(0, 0);
-        display.println(order);
-        display.println("Order Total     ");
-        char total[16];
-        sprintf(total, "Rs.%d", order_total);
-        display.println(total);
-        display.display();
-        order_total = 0;
-        lineNum = 0;
-        return;
-        break;
-    case 'D':
-        strcpy(order, "Order Cancelled");
-        display.clearDisplay();
-        display.setTextSize(1);
-        display.setTextColor(SSD1306_WHITE);
-        display.setCursor(0, 0);
-        display.println(order);
-        display.display();
-        lineNum = 0;
-        order_total = 0;
-        break;
-    default:
-        return;
-        break;
-    }
-    // display.clearDisplay();
-    display.setTextSize(1);
-    display.setTextColor(SSD1306_WHITE);
-    display.setCursor(0, 0);
-    for (int i = 0; i < lineNum; i++)
-    {
-        display.println("");
-    }
-    display.println(order);
-    display.display();
-}
-
-// Display Menu Function
-// void displayMenu(void)
-// {
-
-//     static int count = 0;
-
-//     switch (count++)
-//     {
-//     case 0:
-//         display("1.Tea     Rs.100", 1);
-//         display("2.Coffee  Rs.150", 2);
-//         break;
-//     case 1:
-//         display("3.Biscuits Rs.40", 1);
-//         display("4.Cookies Rs.250", 2);
-//         break;
-//     case 2:
-//         display("5.Roti     Rs.30", 1);
-//         display("6.Lunch   Rs.200", 2);
-//         break;
-//     case 3:
-//         display("7.Dinner  Rs.250", 1);
-//         display("8.BreakfastRs.90", 2);
-//         break;
-//     case 4:
-//         display("9.Snacks  Rs.100", 1);
-//         display("0.Water    Rs.20", 2);
-//         break;
-//     case 5:
-//         display("# to Confirm    ", 1);
-//         display("* to Cancel     ", 2);
-//         break;
-//     case 6:
-//         display("Place Order     ", 1);
-//         display("UwU             ", 2);
-//         break;
-//     default:
-//         count = 0;
-//         break;
-//     }
-// }
-
-// Receiver Code
-#include <esp_now.h>
-#include <WiFi.h>
 
 #define ROW_NUM 4    // four rows
 #define COLUMN_NUM 4 // four columns
@@ -177,40 +22,42 @@ char keys[ROW_NUM][COLUMN_NUM] = {
     {'7', '8', '9', 'C'},
     {'*', '0', '#', 'D'}};
 
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
 byte pin_rows[ROW_NUM] = {19, 18, 5, 17};    // GPIO19, GPIO18, GPIO5, GPIO17 connect to the row pins
 byte pin_column[COLUMN_NUM] = {16, 4, 0, 2}; // GPIO16, GPIO4, GPIO0, GPIO2 connect to the column pins
 
 Keypad keypad = Keypad(makeKeymap(keys), pin_rows, pin_column, ROW_NUM, COLUMN_NUM);
+uint8_t broadcastAddress1[] = {0x9C, 0x9C, 0x1F, 0xC4, 0xFA, 0x80};
 
-// Structure example to receive data
-// Must match the sender structure
-typedef struct test_struct
+typedef struct data_struct
 {
     char key;
-} test_struct;
+} data_struct;
 
-// Create a struct_message called myData
-test_struct myData;
+data_struct test;
 
-// callback function that will be executed when data is received
-void OnDataRecv(const uint8_t *mac, const uint8_t *incomingData, int len)
+esp_now_peer_info_t peerInfo;
+
+// callback when data is sent
+void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status)
 {
-    // display.clearDisplay();
-    memcpy(&myData, incomingData, sizeof(myData));
-    display.setTextSize(1);
-    display.setTextColor(SSD1306_WHITE);
-    display.setCursor(0, 0);
-    // display.println(myData.key);
-    processOrder(myData.key);
-    display.display();
+    char macStr[18];
+    Serial.print("Packet to: ");
+    // Copies the sender mac address to a string
+    snprintf(macStr, sizeof(macStr), "%02x:%02x:%02x:%02x:%02x:%02x",
+             mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
+    Serial.print(macStr);
+    Serial.print(" send status:\t");
+    Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
 }
 
 void setup()
 {
-    // Initialize Serial Monitor
     Serial.begin(115200);
 
-    // Initialize the OLED display
+    WiFi.mode(WIFI_STA);
+
     if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS))
     {
         Serial.println(F("SSD1306 allocation failed"));
@@ -218,164 +65,68 @@ void setup()
             ;
     }
 
-    // Display the text
-    display.display();
-    delay(2000);
-    display.clearDisplay();
-    display.setTextSize(1);
-    display.setTextColor(SSD1306_WHITE);
-    display.setCursor(0, 0);
-    // display.println("Hello, world!");
-    display.display();
-
-    // Set device as a Wi-Fi Station
-    WiFi.mode(WIFI_STA);
-
-    // Init ESP-NOW
     if (esp_now_init() != ESP_OK)
     {
         Serial.println("Error initializing ESP-NOW");
         return;
     }
 
-    // Once ESPNow is successfully Init, we will register for recv CB to
-    // get recv packer info
-    esp_now_register_recv_cb(OnDataRecv);
+    esp_now_register_send_cb(OnDataSent);
+
+    // register peer
+    peerInfo.channel = 0;
+    peerInfo.encrypt = false;
+    // register first peer
+    memcpy(peerInfo.peer_addr, broadcastAddress1, 6);
+    if (esp_now_add_peer(&peerInfo) != ESP_OK)
+    {
+        Serial.println("Failed to add peer");
+        return;
+    }
+
+    display.display();
+    delay(2000);
+    display.clearDisplay();
+    display.setTextSize(1);
+    display.setTextColor(SSD1306_WHITE);
+    display.setCursor(0, 0);
+    // display menu items
+    display.println("1.Tea     Rs.100");
+    display.println("2.Coffee  Rs.150");
+    display.println("3.Biscuits Rs.40");
+    display.println("4.Cookies Rs.250");
+    display.println("5.Roti    Rs.30");
+    display.println("6.Lunch   Rs.200");
+    display.println("C for Confirm");
+    display.println("D for Cancel");
+    display.display();
 }
 
+int millisdelay = millis();
+esp_err_t result;
 void loop()
 {
-    // Display the text received
+    test.key = keypad.getKey();
+
+    if (test.key)
+    {
+        if (millis() - millisdelay > 1000)
+        {
+            millisdelay = millis();
+            result = esp_now_send(broadcastAddress1, (uint8_t *)&test, sizeof(test));
+        }
+        else
+        {
+            millisdelay = millis();
+        }
+    }
+
+    if (result == ESP_OK)
+    {
+        Serial.println("Sent with success");
+    }
+    else
+    {
+        Serial.println("Error sending the data");
+    }
 }
-
-// Transmitter Code
-
-// #include <esp_now.h>
-// #include <WiFi.h>
-
-// #define SCREEN_WIDTH 128 // OLED display width, in pixels
-// #define SCREEN_HEIGHT 64 // OLED display height, in pixels
-
-// #define OLED_RESET -1       // Reset pin # (or -1 if sharing Arduino reset pin)
-// #define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
-
-// #define ROW_NUM 4    // four rows
-// #define COLUMN_NUM 4 // four columns
-
-// char keys[ROW_NUM][COLUMN_NUM] = {
-//     {'1', '2', '3', 'A'},
-//     {'4', '5', '6', 'B'},
-//     {'7', '8', '9', 'C'},
-//     {'*', '0', '#', 'D'}};
-
-// Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
-
-// byte pin_rows[ROW_NUM] = {19, 18, 5, 17};    // GPIO19, GPIO18, GPIO5, GPIO17 connect to the row pins
-// byte pin_column[COLUMN_NUM] = {16, 4, 0, 2}; // GPIO16, GPIO4, GPIO0, GPIO2 connect to the column pins
-
-// Keypad keypad = Keypad(makeKeymap(keys), pin_rows, pin_column, ROW_NUM, COLUMN_NUM);
-
-// // REPLACE WITH YOUR ESP RECEIVER'S MAC ADDRESS
-// // ESP Board MAC Address : 9C : 9C : 1F : C4 : FA : 80
-// uint8_t broadcastAddress1[] = {0x9C, 0x9C, 0x1F, 0xC4, 0xFA, 0x80};
-
-// typedef struct test_struct
-// {
-//     char key;
-// } test_struct;
-
-// test_struct test;
-
-// esp_now_peer_info_t peerInfo;
-
-// // callback when data is sent
-// void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status)
-// {
-//     char macStr[18];
-//     Serial.print("Packet to: ");
-//     // Copies the sender mac address to a string
-//     snprintf(macStr, sizeof(macStr), "%02x:%02x:%02x:%02x:%02x:%02x",
-//              mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
-//     Serial.print(macStr);
-//     Serial.print(" send status:\t");
-//     Serial.println(status == ESP_NOW_SEND_SUCCESS ? "Delivery Success" : "Delivery Fail");
-// }
-
-// void setup()
-// {
-//     Serial.begin(115200);
-
-//     WiFi.mode(WIFI_STA);
-
-//     if (!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS))
-//     {
-//         Serial.println(F("SSD1306 allocation failed"));
-//         for (;;)
-//             ;
-//     }
-
-//     if (esp_now_init() != ESP_OK)
-//     {
-//         Serial.println("Error initializing ESP-NOW");
-//         return;
-//     }
-
-//     esp_now_register_send_cb(OnDataSent);
-
-//     // register peer
-//     peerInfo.channel = 0;
-//     peerInfo.encrypt = false;
-//     // register first peer
-//     memcpy(peerInfo.peer_addr, broadcastAddress1, 6);
-//     if (esp_now_add_peer(&peerInfo) != ESP_OK)
-//     {
-//         Serial.println("Failed to add peer");
-//         return;
-//     }
-
-//     display.display();
-//     delay(2000);
-//     display.clearDisplay();
-//     display.setTextSize(1);
-//     display.setTextColor(SSD1306_WHITE);
-//     display.setCursor(0, 0);
-//     // display menu items
-//     display.println("1.Tea     Rs.100");
-//     display.println("2.Coffee  Rs.150");
-//     display.println("3.Biscuits Rs.40");
-//     display.println("4.Cookies Rs.250");
-//     display.println("5.Roti    Rs.30");
-//     display.println("6.Lunch   Rs.200");
-//     display.println("C for Confirm");
-//     display.println("D for Cancel");
-//     display.display();
-// }
-
-// int millisdelay = millis();
-// esp_err_t result;
-// void loop()
-// {
-//     test.key = keypad.getKey();
-
-//     if (test.key)
-//     {
-//         if (millis() - millisdelay > 1000)
-//         {
-//             millisdelay = millis();
-//             result = esp_now_send(broadcastAddress1, (uint8_t *)&test, sizeof(test));
-//         }
-//         else
-//         {
-//             millisdelay = millis();
-//         }
-//     }
-
-//     if (result == ESP_OK)
-//     {
-//         Serial.println("Sent with success");
-//     }
-//     else
-//     {
-//         Serial.println("Error sending the data");
-//     }
-// }
